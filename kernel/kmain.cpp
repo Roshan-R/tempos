4ac9fa2c74988769292647d023569630e3e5e436
Roshan R Chandar <roshanr2001@gmail.com>
2021-11-05 20:42:08 +0530

    ui : changed bootscreen

--- kernel/kmain.cpp
+++ kernel/kmain.cpp
@@ -6,6 +6,8 @@ extern "C" void kmain()
 {
 	/* Initialize terminal interface */
 	terminal_initialize();
-    terminal_setcolor(VGA_COLOR_CYAN);
-    terminal_writestring("Tesing Makefile");
+    terminal_fill(VGA_COLOR_BLUE);
+    terminal_setcolor(VGA_COLOR_BLUE, VGA_COLOR_LIGHT_MAGENTA);
+    terminal_center_text("Welcome to the awesome world of VGA Graphics!");
 }
+
